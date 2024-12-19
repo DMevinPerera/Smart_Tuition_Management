dbb0cbd648d32d2f1fbf3264be3a15fc8ff2135c
MevinPerera <87105802+DMevinPerera@users.noreply.github.com>
2024-12-19 22:27:40 +0530

    Changed the Port Configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-// vite.config.js
-export default {
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000, // Specify the port number here
+  },
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'] // Example: Split third-party libraries into a separate chunk
-        }
-      }
-    }
-  }
-}
-
+          vendor: ['react', 'react-dom'], // Example: Split third-party libraries into a separate chunk
+        },
+      },
+    },
+  },
+});
