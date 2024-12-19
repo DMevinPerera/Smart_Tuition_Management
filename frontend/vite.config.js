9ad622002e94a62361a0591fedce6e221a4828db
MevinPerera <87105802+DMevinPerera@users.noreply.github.com>
2024-12-19 22:42:16 +0530

    Changed the port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-
+// vite.config.js
+export default {
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'], // Example: Split third-party libraries into a separate chunk
-        },
-      },
-    },
-  },
-});
+          vendor: ['react', 'react-dom'] // Example: Split third-party libraries into a separate chunk
+        }
+      }
+    }
+  }
+}
